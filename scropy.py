6b1f1ea28635f3c6500bdd870ad2251b16352732
Nesseref <orc@frogbox.es>
2015-11-25 18:10:17 -0800

    comment

--- scropy.py
+++ scropy.py
@@ -3,12 +3,13 @@ import clipboard
 import requests
 import datetime
 
+# Local location to save screenshots
 directory = os.path.expanduser("~/screenshots/")
+
 filename = "Screenshot_" + datetime.datetime.now().strftime("%m-%d-%y_%I-%M%p") + ".png"
 path = os.path.join(directory, filename)
 if not os.path.exists(directory):
         os.makedirs(directory)
-
 os.system("scrot -s " + path)
 
 file = {'file': open(path, 'rb')}
@@ -16,4 +17,3 @@ url = "http://frogbox.es/whff/upload.php?raw"
 r = requests.post(url, files=file)
 
 clipboard.copy("http://i.frogbox.es/" + r.text + ".png")
-
